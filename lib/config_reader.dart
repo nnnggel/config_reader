bfbb41c69214193a13614a0f289c482c102ca019
nnnggel <aA10101100>
2020-05-15 16:53:20 +0800

    json

--- lib/config_reader.dart
+++ lib/config_reader.dart
@@ -5,7 +5,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ConfigReader {
-
   static Future<Map<String, String>> readProperties(String content) async {
     if (content?.isEmpty ?? true) {
       return Map<String, String>();
@@ -25,26 +24,36 @@ class ConfigReader {
 
 //    print('propertiesMap.keys: ${propertiesMap.keys.join(',')}');
 //    print('propertiesMap.values: ${propertiesMap.values.join(',')}');
-//    print(
-//        'propertiesMap[app.min-support-version]: ${propertiesMap['app.min-support-version']}');
-//    Fluttertoast.showToast(
-//        msg: 'read ${propertiesMap.keys.length} properties',
-//        timeInSecForIosWeb: 4);
 
     return propertiesMap;
   }
 
   static Future<Map<String, String>> readOnlineProperties(String url) async {
+    return readProperties(await _getUrlContent(url));
+  }
+
+  static Future<Map<String, dynamic>> readJson(String content) async {
+    if (content?.isEmpty ?? true) {
+      return Map<String, dynamic>();
+    }
+    return jsonDecode(content);
+  }
+
+  static Future<Map<String, dynamic>> readOnlineJson(String url) async {
+    return readJson(await _getUrlContent(url));
+  }
+
+  static Future<String> _getUrlContent(String url) async {
     if (url?.isEmpty ?? true) {
-      return Map<String, String>();
+      return '';
     }
     String content;
     try {
       content = Utf8Decoder().convert(await http.readBytes(url));
-//    print('content: content');
+//      print('content: $content');
     } catch (e) {
       throw e;
     }
-    return readProperties(content);
+    return content;
   }
 }
--- test/config_reader_test.dart
+++ test/config_reader_test.dart
@@ -2,7 +2,6 @@ import 'package:config_reader/config_reader.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   test('ConfigReader:readProperties', () async {
     Map<String, String> map = await ConfigReader.readProperties("""
     # SOME COMMENTS
@@ -13,8 +12,20 @@ void main() {
   });
 
   test('ConfigReader:readOnlineProperties', () async {
-    Map<String, String> map = await ConfigReader.readOnlineProperties('https://github.com/nnnggel/config_reader/blob/master/test/testfile/test.properties');
+    Map<String, String> map = await ConfigReader.readOnlineProperties(
+        'https://raw.githubusercontent.com/nnnggel/config_reader/master/test/testfile/test.properties');
     expect(map['website'], 'https://github.com/nnnggel/config_reader');
   });
 
+  test('ConfigReader:readJson', () async {
+    Map<String, dynamic> map = await ConfigReader.readJson(
+        '{"website": "https://github.com/nnnggel/config_reader", "version": 1}');
+    expect(map['website'], 'https://github.com/nnnggel/config_reader');
+  });
+
+  test('ConfigReader:readOnlineJson', () async {
+    Map<String, dynamic> map = await ConfigReader.readOnlineJson(
+        'https://raw.githubusercontent.com/nnnggel/config_reader/master/test/testfile/test.json');
+    expect(map['version'], 1);
+  });
 }
